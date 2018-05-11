a22856a9ea6de9976fadd2d6dc63acc85b89015b
ruochen <liruochen361@163.com>
2018-05-11 16:28:17 +0800

    Hustrix

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/atguigu/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * User: ruochen
+ * Date:2018/5/11 0011
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -0,0 +1,57 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * User: ruochen
+ * Date:2018/5/9 0009
+ */
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+
+    @RequestMapping(value = "dept/add",method = RequestMethod.POST)
+    public boolean add(Dept dept){
+        return deptService.add(dept);
+    }
+
+    @RequestMapping(value="/dept/get/{id}",method=RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id)
+    {
+        Dept dept =  this.deptService.get(id);
+        if(null == dept)
+        {
+            throw new RuntimeException("该ID："+id+"没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") Long id)
+    {
+        return new Dept().setDeptno(id)
+                .setDname("该ID："+id+"没有没有对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
+    }
+
+
+    @RequestMapping(value = "dept/list",method = RequestMethod.GET)
+    public List<Dept> list(){
+        return deptService.list();
+    }
+
+
+
+}
